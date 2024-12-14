885d1615abb056064c2d81ff119d889dc88ef77e
poomp5 <poom27200@gmail.com>
2024-12-15 04:18:13 +0700

    history m2

--- app/m2/midterm2/page.tsx
+++ app/m2/midterm2/page.tsx
@@ -137,6 +137,43 @@ export default function Home() {
                                             14/12/2024
                                         </td>
                                     </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
+                                            <Link href={'/m2/midterm2/history.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/history.png"
+                                                        alt="math"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        ประวัติศาสตร์
+                                                    </p>
+                                                    <p className="text-xs text-gray-600 text-nowrap">
+                                                        สอบกลางภาค (by <Link className="text-purple-800" href="https://www.instagram.com/zong_angpao/">zong_angpao</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.2 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m2/midterm2/history.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            15/12/2024
+                                        </td>
+                                    </tr>
                                     
                                 </tbody>
                             </table>
