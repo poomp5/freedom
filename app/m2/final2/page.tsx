a7f81b60bbc5c5d0294050a6fdf7396ca3d387d2
poomp5 <poom27200@gmail.com>
2025-02-20 20:54:34 +0700

    update m2 science new

--- app/m2/final2/page.tsx
+++ app/m2/final2/page.tsx
@@ -48,12 +48,12 @@ export default function Home() {
                                                     </p>
                                                 </div>
                                             </Link>
-                                        </td>
+                                          </td>
                                         <td className="px-4 py-3 text-sm whitespace-nowrap">
                                             ม.2 เทอม 2
                                         </td>
                                         <td className="px-4 py-3 text-xs">
-                                            <Link href={'/m2/final2/math.pdf'}>
+                                            <Link href={'/m2/final2/math_new.pdf'}>
                                                 <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
                                                     ดาวน์โหลด
                                                 </span>
@@ -98,6 +98,43 @@ export default function Home() {
                                         </td>
                                         <td className="px-2 py-3 text-sm">
                                             17/11/2024
+                                        </td>
+                                    </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
+                                            <Link href={'/m2/final2/sci_new.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/sci.png"
+                                                        alt="math"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        วิทยาศาสตร์ (ใหม่)
+                                                    </p>
+                                                    <p className="text-xs text-gray-600">
+                                                        สอบปลายภาค (by <Link className="text-purple-800" href="https://www.instagram.com/meemmiiimi/">meemmiiimi</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.2 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m2/final2/sci_new.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            20/02/2025
                                         </td>
                                     </tr>
                                     <tr className="text-gray-700">
