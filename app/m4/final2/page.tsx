44727bc37df41be9de6414ddfb3c2ad8af24b9f0
poomp5 <poom27200@gmail.com>
2025-02-23 18:24:04 +0700

    add english m4

--- app/m4/final2/page.tsx
+++ app/m4/final2/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
                                                     <Image
                                                         className="icon-subject object-cover w-full h-auto"
                                                         src="/assets/img/icon/social.png"
-                                                        alt="math"
+                                                        alt="social"
                                                         width={100}
                                                         height={100}
                                                     />
@@ -144,7 +144,7 @@ export default function Home() {
                                                     <Image
                                                         className="icon-subject object-cover w-full h-auto"
                                                         src="/assets/img/icon/physics.png"
-                                                        alt="math"
+                                                        alt="physics"
                                                         width={100}
                                                         height={100}
                                                     />
@@ -171,7 +171,44 @@ export default function Home() {
                                             </Link>
                                         </td>
                                         <td className="px-2 py-3 text-sm">
-                                            22/02/2025
+                                            23/02/2025
+                                        </td>
+                                    </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
+                                            <Link href={'/m4/final2/eng.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/english.png"
+                                                        alt="eng"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        ภาษาอังกฤษ
+                                                    </p>
+                                                    <p className="text-xs text-gray-600">
+                                                        สอบปลายภาค (by <Link className="text-purple-800" href="https://www.instagram.com/creammyeiieee__/">creammyeiieee__</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.4 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m4/final2/eng.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            23/02/2025
                                         </td>
                                     </tr>
                                 </tbody>
