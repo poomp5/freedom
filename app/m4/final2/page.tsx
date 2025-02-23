c73d6974ea69fab73f21318f8771cb186a31aa9c
poomp5 <poom27200@gmail.com>
2025-02-23 10:01:30 +0700

    math_extra m4

--- app/m4/final2/page.tsx
+++ app/m4/final2/page.tsx
@@ -60,7 +60,44 @@ export default function Home() {
                                             </Link>
                                         </td>
                                         <td className="px-2 py-3 text-sm">
-                                            22/02/2024
+                                            22/02/2025
+                                        </td>
+                                    </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
+                                            <Link href={'/m4/final2/math_extra.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/math.png"
+                                                        alt="math"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        คณิตศาสตร์ (เพิ่มเติม)
+                                                    </p>
+                                                    <p className="text-xs text-gray-600">
+                                                        สอบปลายภาค (by <Link className="text-purple-800" href="https://www.instagram.com/torgor_xlt.09">torgor_xlt.09</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.4 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m4/final2/math_extra.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            23/02/2025
                                         </td>
                                     </tr>
                                     <tr className="text-gray-700">
@@ -97,7 +134,7 @@ export default function Home() {
                                             </Link>
                                         </td>
                                         <td className="px-2 py-3 text-sm">
-                                            22/02/2024
+                                            22/02/2025
                                         </td>
                                     </tr>
                                 </tbody>
