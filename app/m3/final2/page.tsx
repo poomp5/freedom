c5ab62178a4523915ce39ddfc4445b82a28135c4
poomp5 <poom27200@gmail.com>
2025-02-23 17:29:12 +0700

    m3 thai

--- app/m3/final2/page.tsx
+++ app/m3/final2/page.tsx
@@ -102,6 +102,43 @@ export default function Home() {
                                     </tr>
                                     <tr className="text-gray-700">
                                         <td className="px-4 py-3">
+                                            <Link href={'/m3/final2/thai_new.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/thai.png"
+                                                        alt="math"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        ภาษาไทย (เนื้อหาวิจัย)
+                                                    </p>
+                                                    <p className="text-xs text-gray-600">
+                                                        สอบปลายภาค (by <Link className="text-purple-800" href="https://discord.gg/Gjcr8XvaFM">ขบวนการมะ</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.3 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m3/final2/thai_new.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            17/11/2024
+                                        </td>
+                                    </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
                                             <Link href={'/m3/final2/thai.pdf'} className="flex items-center text-sm">
                                                 <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
                                                     <Image
@@ -128,7 +165,7 @@ export default function Home() {
                                         </td>
                                         <td className="px-4 py-3 text-xs">
                                             <Link href={'/m3/final2/thai.pdf'}>
-                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                <span className="px-2 py-1 font-semibold leading-tight text-gray-700 bg-gray-100 rounded-full">
                                                     ดาวน์โหลด
                                                 </span>
                                             </Link>
