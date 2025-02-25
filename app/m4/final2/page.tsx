18ca0ac7ee1fd5fea69ec2545d610835aa389159
poomp5 <poom27200@gmail.com>
2025-02-25 15:48:48 +0700

    sci + chemis

--- app/m4/final2/page.tsx
+++ app/m4/final2/page.tsx
@@ -139,6 +139,80 @@ export default function Home() {
                                     </tr>
                                     <tr className="text-gray-700">
                                         <td className="px-4 py-3">
+                                            <Link href={'/m4/final2/chemistry.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/chemistry.png"
+                                                        alt="biology"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        เคมี
+                                                    </p>
+                                                    <p className="text-xs text-gray-600">
+                                                        สอบปลายภาค (by <Link className="text-purple-800" href="https://www.instagram.com/torgor_xlt.09">torgor_xlt.09</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.4 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m4/final2/chemistry.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            25/02/2025
+                                        </td>
+                                    </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
+                                            <Link href={'/m4/final2/sci.pdf'} className="flex items-center text-sm">
+                                                <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
+                                                    <Image
+                                                        className="icon-subject object-cover w-full h-auto"
+                                                        src="/assets/img/icon/sci.png"
+                                                        alt="biology"
+                                                        width={100}
+                                                        height={100}
+                                                    />
+                                                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div className="inline-block">
+                                                    <p className="font-semibold whitespace-nowrap">
+                                                        วิทยาศาสตร์
+                                                    </p>
+                                                    <p className="text-xs text-gray-600">
+                                                        สอบปลายภาค (by <Link className="text-purple-800" href="https://www.instagram.com/d3w4r_zz/">d3w4r_zz</Link>)
+                                                    </p>
+                                                </div>
+                                            </Link>
+                                        </td>
+                                        <td className="px-4 py-3 text-sm whitespace-nowrap">
+                                            ม.4 เทอม 2
+                                        </td>
+                                        <td className="px-4 py-3 text-xs">
+                                            <Link href={'/m4/final2/sci.pdf'}>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
+                                                    ดาวน์โหลด
+                                                </span>
+                                            </Link>
+                                        </td>
+                                        <td className="px-2 py-3 text-sm">
+                                            25/02/2025
+                                        </td>
+                                    </tr>
+                                    <tr className="text-gray-700">
+                                        <td className="px-4 py-3">
                                             <Link href={'/m4/final2/social.pdf'} className="flex items-center text-sm">
                                                 <div className="inline-block relative w-8 h-8 mr-3 rounded-full">
                                                     <Image
