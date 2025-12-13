8fea79c98f8ae6b3a8b95b9d2817817e39cdf9d0
poomp5 <poom27200@gmail.com>
2025-12-13 14:59:03 +0700

    feat: add new biology exam sheet for midterm 2 with relevant details

--- app/m5/midterm2/page.tsx
+++ app/m5/midterm2/page.tsx
@@ -19,6 +19,17 @@ const sheets = [
     },
   },
   {
+    subject: "ชีววิทยา (แนวข้อสอบ)",
+    filename: "/m5/midterm2/biology-exam.pdf",
+    icon: "/assets/img/icon/biology.png",
+    date: "13/12/2025",
+    term: "ม.5 เทอม 2",
+    by: {
+      name: "torgor_xlt.09",
+      url: "https://www.instagram.com/torgor_xlt.09",
+    },
+  },
+  {
     subject: "ชีววิทยา (67)",
     filename: "/m5/midterm2/biology-67.pdf",
     icon: "/assets/img/icon/biology.png",
@@ -29,6 +40,7 @@ const sheets = [
       url: "https://www.instagram.com/ppuinoon.s",
     },
   },
+
   {
     subject: "วิทยาศาสตร์ (สายศิลป์)",
     filename: "/m5/midterm2/sci.pdf",
